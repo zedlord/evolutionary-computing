9bb3e222d091fd3f7f47a28af44ccaed907e81a7
zedlord <bettydaan@gmail.com>
2017-10-16 13:32:33 +0200

    Update test.bat
    wat er een beetje maf fout gaat

--- test.bat
+++ test.bat
@@ -2,15 +2,15 @@ echo BentCigarFunction >>BentCigar.txt
 FOR /l %%A IN (1,1,20) DO (
     echo %%A>>BentCigar.txt
     FOR /l %%B IN (1,1,20) DO (
-        java -jar testrun.jar -submission=player02Tournament -evaluation=BentCigarFunction -seed=%%A >>BentCigar.txt
+        java -jar testrun.jar -submission=player02Particle -evaluation=BentCigarFunction -seed=%%A >>BentCigar.txt
     )
 )
 
-echo SchaffersEvaluation >>Schaffers.txt
-FOR /l %%A IN (1,1,20) DO (
-    SET seed=%%A
-    echo %seed% >>Schaffers.txt
-    FOR /l %%B IN (1,1,20) DO (
-        java -jar testrun.jar -submission=player02Tournament -evaluation=SchaffersEvaluation -seed=%%A >>Schaffers.txt
-    )
-)
+REM echo SchaffersEvaluation >>Schaffers.txt
+REM FOR /l %%A IN (1,1,20) DO (
+REM     SET seed=%%A
+REM     echo %seed% >>Schaffers.txt
+REM     FOR /l %%B IN (1,1,20) DO (
+REM         java -jar testrun.jar -submission=player02Tournament -evaluation=SchaffersEvaluation -seed=%%A >>Schaffers.txt
+REM     )
+REM )
